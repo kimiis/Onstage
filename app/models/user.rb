84766bac3452eb51932c04a48ae0745bea75f546
kim isabelle <isabelle.kim2023@campus-eni.fr>
2024-06-05 15:16:19 +0200

    fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :sent_messages, class_name: 'Message', foreign_key: 'sender_id'
   has_many :received_messages, class_name: 'Message', foreign_key: 'receiver_id'
+  
   has_many :stages
   has_many :artists
   has_many :ads
