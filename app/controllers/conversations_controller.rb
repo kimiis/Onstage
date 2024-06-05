5ac5f66864935dcab970b2fa9ec001f1b4f97d60
Juan Vianney NM <juanvianneynm@gmail.com>
2024-06-05 17:10:02 +0200

    conversation is ready 😊

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -6,6 +6,12 @@ class ConversationsController < ApplicationController
   def show
     @conversation = Conversation.find(params[:id])
     @messages = Message.where(conversation: @conversation)
+    @message = Message.new
+    @message.conversation = @conversation
+    @message.sender = current_user
+    @message.receiver = @conversation.messages.first.sender == current_user ?
+      @conversation.messages.first.receiver :
+      @conversation.messages.first.sender
   end
 
   def create
