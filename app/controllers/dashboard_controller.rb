b036a2d570a98c4130c9b12ecf3ad0c9808ab0c9
kim isabelle <isabelle.kim2023@campus-eni.fr>
2024-06-07 11:48:01 +0200

    test

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,14 +4,8 @@ class DashboardController < ApplicationController
     @best_stages = Booking.best_stages
     @last_ads = Booking.last_ads
     # raise
-<<<<<<< HEAD
-    # @artist = Artist.find_by(user: current_user)
-    # @artist_id = @artist.id
-
-=======
     @artist = Artist.find_by(user: current_user)
     @artist_id = @artist.id
->>>>>>> master
     # @stage = Stage.find_by(user: current_user)
     # @stage_id = @stage.id
 
