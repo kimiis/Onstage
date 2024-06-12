f650090ae25481dc3d80209041a5167e3c2a19b5
Yoann Tourtellier <tourtellier.yoann@gmail.com>
2024-06-12 11:17:13 +0200

    validation d'image en place

--- app/models/artist_ad.rb
+++ app/models/artist_ad.rb
@@ -3,4 +3,6 @@ class ArtistAd < ApplicationRecord
   pg_search_scope :search_by_content, against: :content, using: { tsearch: { prefix: true } }
   has_one_attached :photo
   belongs_to :artist
+
+  validates :photo, presence: { message: "Veuillez insérer une photo" }
 end
